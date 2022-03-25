a933bd273bbe0931739cb1867f06369d5f396270
Riyamittal15 <riya17367@gmail.com>
2022-03-25 21:40:55 +0530

    sparshi pleaseeeeeeee accept these changes

--- src/components/ProductListing.js
+++ src/components/ProductListing.js
@@ -1,12 +1,14 @@
-import React, { useEffect } from "react";
+import React, { useEffect,useState } from "react";
 import axios from "axios";
 import { useDispatch, useSelector } from "react-redux";
 import { setProducts } from "../redux/actions/productActions";
 import ProductComponent from "./ProductComponent";
 import classes from "./ProductListing.module.css";
+import Loading from "./Loading";
 const ProductPage = () => {
   const products = useSelector((state) => state.allProducts.products);
   const dispatch = useDispatch();
+  const [isloading,setloading]=useState(true);
   const fetchProducts = async () => {
     const response = await axios
       .get("https://fakestoreapi.com/products")
@@ -14,6 +16,7 @@ const ProductPage = () => {
         console.log("Err: ", err);
       });
     dispatch(setProducts(response.data));
+      setloading(false);
   };
 
   useEffect(() => {
@@ -23,7 +26,7 @@ const ProductPage = () => {
   console.log("Products :", products);
   return (
     <div className={classes.all_cards}>
-      <ProductComponent />
+      {isloading?<Loading/>:<ProductComponent />}
     </div>
   );
 };
